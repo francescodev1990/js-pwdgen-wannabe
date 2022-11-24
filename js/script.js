4712105c9427782f5e2629cc2c30f8656e7cc33f
Your Name <almannofrancesco@gmail.com>
2022-11-24 23:24:32 +0100

    js modified

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+let name_user = prompt('Per favore inserisci il tuo nome');
+console.log(name_user);
+
+document.getElementById("salutare").innerHTML = 'Ciao' + " " + name_user;
+console.log('Ciao + name_user');
+
+let surname_user = prompt('Per favore inserisci il tuo cognome');
+console.log(surname_user);
+
+
+//CONCATENAZIONE//
+let conc = name_user + " " + surname_user;
+console.log(conc);
+//CONCATENAZIONE//
+
+let color = prompt('Per favore inserisci il tuo colore preferito');
+console.log(color);
+
+let number = prompt('Per favore inserisci il numero 21');
+console.log(number);
+
