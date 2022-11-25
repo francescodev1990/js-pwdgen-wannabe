d8cc60f9ad331cbdf7fafa9baf42c99a5e330d7a
Your Name <almannofrancesco@gmail.com>
2022-11-25 09:48:40 +0100

    completed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+
 let name_user = prompt('Per favore inserisci il tuo nome');
 console.log(name_user);
 
@@ -8,15 +9,15 @@ console.log('Ciao + name_user');
 let surname_user = prompt('Per favore inserisci il tuo cognome');
 console.log(surname_user);
 
-
-//CONCATENAZIONE//
-let conc = name_user + " " + surname_user;
-console.log(conc);
-//CONCATENAZIONE//
-
 let color = prompt('Per favore inserisci il tuo colore preferito');
 console.log(color);
 
 let number = prompt('Per favore inserisci il numero 21');
-console.log(number);
+console.log(number)
 
+//CONCATENAZIONE//
+let concat = name_user + surname_user + color + 21;
+console.log(concat);
+//CONCATENAZIONE//
+
+document.getElementById('passwordinsicura').innerHTML = concat;
